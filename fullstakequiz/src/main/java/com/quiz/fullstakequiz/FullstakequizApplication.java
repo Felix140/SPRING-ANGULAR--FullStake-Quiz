e46679b91912c590751bf20a67a4104cddb5678f
Felix Valdez <fdvaldez14@gmail.com>
2023-08-16 16:58:51 +0200

    Add  SpringInitialzr Project

---
+++ fullstakequiz/src/main/java/com/quiz/fullstakequiz/FullstakequizApplication.java
@@ -0,0 +1,13 @@
+package com.quiz.fullstakequiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstakequizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstakequizApplication.class, args);
+	}
+
+}
---
+++ fullstakequiz/src/test/java/com/quiz/fullstakequiz/FullstakequizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiz.fullstakequiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstakequizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
