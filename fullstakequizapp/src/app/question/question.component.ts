e7532bb4f633ab78fb612c2f9e5092237a7773d2
Felix Valdez <fdvaldez14@gmail.com>
2023-08-24 18:22:14 +0200

    Aggiunta logica a QuestionComponent + html
    nextQuestion + previousQuestion + progressBar + Current Question

--- fullstakequizapp/src/app/question/question.component.ts
+++ fullstakequizapp/src/app/question/question.component.ts
@@ -8,7 +8,17 @@ import { QuestionService } from '../service/question.service';
 })
 export class QuestionComponent implements OnInit{
 
-  public name : string="";
+  public name: string="";
+  public questionList: any= [];
+  public currentQuestion: number = 0;
+  // Score
+  public points: number = 0;
+  // Timer
+  public timer: number = 0;
+  // ProgressBar
+  public progress: number = 10;
+  
+
   constructor(private questionService: QuestionService) {}
 
   ngOnInit(): void {
@@ -21,7 +31,20 @@ export class QuestionComponent implements OnInit{
     this.questionService.getQuestions()
     .subscribe(res => {
       console.log(res);
+      this.questionList = res;
     })
+  }
+  
+  // PROSSIMA DOMANDA
+  nextQuestion() {
+    this.currentQuestion++;
+    this.progress += 10;
+  }
+
+  // DOMANDA PRECEDENTE
+  previousQuestion() {
+    this.currentQuestion--;
+    this.progress -= 10;
   }
 
 }
