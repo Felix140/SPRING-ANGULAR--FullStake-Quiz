ddb5f7d2dc0ec1669c678765cd92eec822f95ba9
Felix Valdez <fdvaldez14@gmail.com>
2023-08-24 15:54:45 +0200

    Username string interpolation nel local storage

--- fullstakequizapp/src/app/welcome/welcome.component.ts
+++ fullstakequizapp/src/app/welcome/welcome.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss']
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements OnInit{
 
+  @ViewChild('username') nameKey!: ElementRef;// deve matchare con #username
+  constructor() {
+
+  }
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  startQuiz() {
+    localStorage.setItem("name", this.nameKey.nativeElement.value);
+  }
 }
