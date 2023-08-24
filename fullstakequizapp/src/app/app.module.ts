45f0d46d1e27150b3ff7ebdda01affec50bb386b
Felix Valdez <fdvaldez14@gmail.com>
2023-08-24 17:00:23 +0200

    Add QUESTION service per richieste HTTP

--- fullstakequizapp/src/app/app.module.ts
+++ fullstakequizapp/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { QuestionComponent } from './question/question.component';
 import { HeaderComponent } from './header/header.component';
+import { QuestionComponent } from './question/question.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ fullstakequizapp/src/app/service/question.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Question } from '../interface/question';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuestionService {
+
+  private readonly apiServerUrl = 'http://localhost:8080';
+
+  // facciamo qui sotto un INJECTION dell'HttpClient nel costruttore (Dependency Injection)
+  constructor(private http: HttpClient) { }
+
+  //* POST
+  public addQuestion(question: Question): Observable<Question[]> {
+    return this.http.post<Question[]>(`${this.apiServerUrl}/quiz/add`, question);
+  }
+
+
+  //* GET (list)
+  public getQuestions(): Observable<Question[]> {
+    return this.http.get<Question[]>(`${this.apiServerUrl}/quiz/all`); //! attenzione: vanno aggiunti i BACKTICK
+  }
+
+
+  //* PUT
+  public updateQuestions(question: Question): Observable<Question[]> {
+    return this.http.put<Question[]>(`${this.apiServerUrl}/quiz/update`, question);
+  }
+
+  //* DELETE
+  public deleteQuestions(questionId: number): Observable<Question[]> {
+    return this.http.delete<Question[]>(`${this.apiServerUrl}/quiz/delete/${questionId}`);
+  }
+}
